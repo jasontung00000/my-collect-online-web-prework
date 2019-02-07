b68ec45c757bd0ef3622f824a16813839c7fa2dd
Jason Tung <jasontung123@yahoo.com>
2019-02-07 06:01:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,8 @@
 def my_collect
-  
+  #languages { ['ruby', 'javascript', 'python', 'objective-c']}
+  students { ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] }
+  my_collect(students) do
+    |name| 
+    name.split("").first
 end
 
